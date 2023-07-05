c202b582c83b36b7a0fe5762a4cdfd87ea3f13f6
isaias junior <isaias.fonseca@hotmail.com>
2023-07-05 11:55:35 -0300

    Adicionando componentes estaticos básicos

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+function Footer() {
+    return (
+
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/isaias-fonseca/" target="_blank" rel="noreferrer">isaias junior</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
