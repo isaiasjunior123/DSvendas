df33f68343554648740752f189bdf0bac0385c9b
isaias junior <isaias.fonseca@hotmail.com>
2023-07-10 18:56:52 -0300

    Integração com o grafico de rosca(DonutChart)

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -2,32 +2,47 @@ import axios from "axios";
 import Chart from "react-apexcharts"
 import { SaleSum } from "types/sale";
 import { BASE_URL } from "utils/requests";
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+
 
 type ChartData = {
-    labels: string [];
-    series: number [];
+    labels: string[];
+    series: number[];
 }
 
 function DonutChart() {
 
-   let chartData : ChartData = {labels:[], series: []};
+    /* Forma errada
+       let chartData : ChartData = {labels:[], series: []};*/
 
-   axios.get(`${BASE_URL}/sales/amount-by-seller`)
-   .then(response =>{
-    const data = response.data as SaleSum[];
-    const myLabels = data.map(x => x.sellerName);
-    const mySeries = data.map(x => x.sum);
+    /* FORMA CORRETA*/
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-    chartData = {labels:myLabels, series: mySeries};
 
-    console.log(chartData);
+    useEffect(() => {
 
-   })
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
 
-   /* const mockData = {
-        series: [477138, 499928, 444867, 220426, 473088],
-        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    } */
+                setChartData({ labels: myLabels, series: mySeries });
+
+
+
+            });
+
+    }, []);
+
+
+
+    /* const mockData = {
+         series: [477138, 499928, 444867, 220426, 473088],
+         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+     } */
 
     const options = {
         legend: {
