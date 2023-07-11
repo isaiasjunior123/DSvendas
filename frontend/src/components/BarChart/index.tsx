ab820cbbc2fab434ddb733aec0f30e4c43692d8f
isaias junior <isaias.fonseca@hotmail.com>
2023-07-11 12:32:44 -0300

    Integração com a tabela de dados

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -75,8 +75,20 @@ function BarChart() {
         },
     };
 
-  
 
+    /* esse contedu não é mais necessario , deixei para estudar
+    const mockData = {
+        labels: {
+            categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+        },
+        series: [
+            {
+                name: "% Sucesso",
+                data: [43.6, 67.1, 67.7, 45.6, 71.1]
+            }
+        ]
+    };
+ */
 
     return (
         <Chart
